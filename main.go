4acad270666a62c5c04bd2e46ade208d6fac8d5c
muthuramanecs03g <muthuramane.cs03g@g2.nctu.edu.tw>
2022-01-06 09:52:29 +0000

    Added more cobra command use cases

--- main.go
+++ main.go
@@ -1,9 +1,105 @@
 package main
 
 import (
-	cmd "github.com/muthuramanecs03g/mycli/cmd"
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd  = &cobra.Command{Use: "gogtpu"}
+	cmdPrint = &cobra.Command{
+		Use:   "print [string to print]",
+		Short: "Print anything to the screen",
+		Long:  `print is for printing anything back to the screen. For many years people have printed back to the screen.`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Print: " + strings.Join(args, " "))
+		},
+	}
+	cmdEcho = &cobra.Command{
+		Use:   "echo [string to echo]",
+		Short: "Echo anything to the screen",
+		Long:  `echo is for echoing anything back. Echo works a lot like print, except it has a child command.`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Echo: " + strings.Join(args, " "))
+		},
+	}
+	echoTimes int
+	cmdTimes  = &cobra.Command{
+		Use:   "times [string to echo]",
+		Short: "Echo anything to the screen more times",
+		Long:  `echo things multiple times back to the user by providing a count and a string.`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			for i := 0; i < echoTimes; i++ {
+				fmt.Println("Echo: " + strings.Join(args, " "))
+			}
+		},
+	}
+	cmdT1 = &cobra.Command{
+		Use:   "root [sub]",
+		Short: "My root command",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside rootCmd PersistentPreRun with args: %v\n", args)
+		},
+		PreRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside rootCmd PreRun with args: %v\n", args)
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside rootCmd Run with args: %v\n", args)
+		},
+		PostRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside rootCmd PostRun with args: %v\n", args)
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside rootCmd PersistentPostRun with args: %v\n", args)
+		},
+	}
+	cmdT1SubCmd = &cobra.Command{
+		Use:   "sub [no options!]",
+		Short: "My subcommand",
+		PreRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside subCmd PreRun with args: %v\n", args)
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside subCmd Run with args: %v\n", args)
+		},
+		PostRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside subCmd PostRun with args: %v\n", args)
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Inside subCmd PersistentPostRun with args: %v\n", args)
+		},
+	}
 )
 
 func main() {
-	cmd.Execute()
+	fmt.Println("Test: Use case1")
+	test1()
+	fmt.Println("Test: Use case2")
+	test2()
+
+	rootCmd.Execute()
+}
+
+func test1() {
+	fmt.Println("test2: Entry")
+	cmdTimes.Flags().IntVarP(&echoTimes, "times", "t", 1, "times to echo the input")
+	rootCmd.AddCommand(cmdPrint, cmdEcho)
+	cmdEcho.AddCommand(cmdTimes)
+}
+
+func test2() {
+	fmt.Println("test2: Entry")
+	rootCmd.AddCommand(cmdT1)
+	rootCmd.AddCommand(cmdT1SubCmd)
+	//rootCmd.SetArgs([]string{""})
+	//rootCmd.Execute()
+
+	//fmt.Println("test2: wiht args")
+	//rootCmd.SetArgs([]string{"sub", "arg1", "arg2"})
+	//rootCmd.Execute()
 }
